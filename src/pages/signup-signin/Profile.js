33243000f6fef6235b2a154413052aa349b4483d
rbkunwar <baburabin45@gmail.com>
2023-11-30 10:32:33 +1100

    digital library

---
+++ src/pages/signup-signin/Profile.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { PrivateRoute } from "../../components/private-route/PrivateRoute";
+import { Container } from "react-bootstrap";
+import { UserLayout } from "../../components/layout/UserLayout";
+
+const Profile = () => {
+  return (
+    <PrivateRoute>
+      <UserLayout>
+        <Container>
+          <h3>Profile</h3>
+
+          <hr />
+        </Container>
+      </UserLayout>
+    </PrivateRoute>
+  );
+};
+
+export default Profile;
